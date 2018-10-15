f998c08b304e9b0e6a999eded4eab35dfd2180ef
Neulana <fupinyou@gmail.com>
2018-10-15 22:44:28 +0800

    Revert "✨ add travis CI"
    This reverts commit b71c99ae9d226ccb4b64fa63dbb5440a0a74a3d4.

--- tests/test_percent.py
+++
@@ -1,8 +0,0 @@
-"""
-Test if the percentage is legal
-"""
-from datebar.date_bar import get_percent
-
-def test_percent():
-    percent_num = get_percent()
-    assert (percent_num >= 0 and percent_num <= 100)
